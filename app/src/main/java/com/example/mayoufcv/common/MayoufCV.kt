0a57ea9b31ceaff65080c724f3a5f81ca0378019
M Mayouf <user@example.com>
2019-03-21 19:37:04 +0000

    Enabled Dagger Android components

--- app/src/main/java/com/example/mayoufcv/common/MayoufCV.kt
+++ app/src/main/java/com/example/mayoufcv/common/MayoufCV.kt
@@ -1,10 +1,17 @@
 package com.example.mayoufcv.common
 
+import android.app.Activity
 import android.app.Application
 import com.example.mayoufcv.di.AppComponent
 import com.example.mayoufcv.di.DaggerAppComponent
+import dagger.android.DispatchingAndroidInjector
+import dagger.android.HasActivityInjector
+import javax.inject.Inject
 
-class MayoufCV : Application() {
+class MayoufCV : Application(), HasActivityInjector {
+
+    @Inject
+    lateinit var dispatchingAndroidInjector: DispatchingAndroidInjector<Activity>
 
     private val appComponent: AppComponent by lazy {
         DaggerAppComponent.builder()
@@ -16,4 +23,6 @@ class MayoufCV : Application() {
         super.onCreate()
         appComponent.inject(this)
     }
+
+    override fun activityInjector() = dispatchingAndroidInjector
 }
---
+++ app/src/main/java/com/example/mayoufcv/di/BuildersModule.kt
@@ -0,0 +1,15 @@
+package com.example.mayoufcv.di
+
+import com.example.mayoufcv.screen.home.HomeActivity
+import com.example.mayoufcv.screen.home.di.HomeActivityModule
+import com.example.mayoufcv.screen.home.di.HomeModule
+import com.example.mayoufcv.screen.home.di.HomeScope
+import dagger.Module
+import dagger.android.ContributesAndroidInjector
+
+@Module
+abstract class BuildersModule {
+    @ContributesAndroidInjector(modules = [HomeModule::class])
+    @HomeScope
+    abstract fun homeActivity(): HomeActivity
+}
