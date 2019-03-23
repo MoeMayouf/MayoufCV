b1a5733d75e2f7e9ada9b13471e60e82ec851ce3
M Mayouf <user@example.com>
2019-03-23 09:06:39 +0000

    Added a custom view to show tech stack

---
+++ app/src/main/java/com/example/mayoufcv/common/TechStackView.kt
@@ -0,0 +1,33 @@
+package com.example.mayoufcv.common
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.TextView
+import androidx.databinding.BindingAdapter
+import com.example.mayoufcv.R
+import com.google.android.flexbox.FlexboxLayout
+import com.google.android.flexbox.FlexboxLayout.*
+import com.google.android.material.button.MaterialButton
+
+class TechStackView @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttribute: Int = 0)
+    : FlexboxLayout(context, attrs, defStyleAttribute) {
+
+    fun populateViews(data: List<String>) {
+        data.forEach {
+            val button = MaterialButton(context)
+            button.text = it
+            val layoutParams = LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT)
+            layoutParams.setMargins(context.resources.getDimensionPixelSize(R.dimen.tech_stack_margin_horizontal),
+                layoutParams.topMargin,
+                context.resources.getDimensionPixelSize(R.dimen.tech_stack_margin_horizontal),
+                layoutParams.bottomMargin)
+            button.layoutParams = layoutParams
+            addView(button)
+        }
+    }
+}
+
+@BindingAdapter("dataset")
+fun setDataset(techStackView: TechStackView, data: List<String>) {
+    techStackView.populateViews(data)
+}
